1068446aaa21e27992923618a1af0ac2dfac7488
NikhilAdyapak <nikhiladyapak31@gmail.com>
2023-03-06 14:22:55 +0530

    update

--- deploy/flask/try.py
+++
@@ -1,5 +0,0 @@
-import os
-import numpy as np
-print(os.path.join(os.getcwd(),'mlops.txt'))
-
-print(np.random.randn(4,))
--- transform.py
+++
@@ -1,4 +0,0 @@
-import scipy.io as sio
-data =  sio.loadmat("car_devkit/devkit/cars_train_annos.mat")
-dataframe = data['annotations'][0]
-print(dataframe)
