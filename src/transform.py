971990423d38859549d6e000eeba2ccd89d3769d
NikhilAdyapak <nikhiladyapak31@gmail.com>
2023-03-09 14:58:38 +0530

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,63 +1,3 @@
-# import sys
-# import os
-# import yaml
-# import pandas as pd
-# import glob
-# import xml.etree.ElementTree as ET
-
-# if len(sys.argv) != 3:
-#     sys.stderr.write('Arguments error. Usage:\n')
-#     sys.stderr.write(
-#         '\tpython3 src/transform.py data/prepared data/transformed\n'
-#     )
-#     sys.exit(1)
-
-# params = yaml.safe_load(open('params.yaml'))['ingest']
-
-# images = os.path.join(sys.argv[1],f"v{params['dcount']}",'images')
-# annots = os.path.join(sys.argv[1],f"v{params['dcount']}",'annotations')
-
-# outputannot = os.path.join(sys.argv[2],f"v{params['dcount']}")
-# os.makedirs(outputannot, exist_ok=True)
-
-# def generate_data( Annotpath, Imagepath):
-#     information={'xmin':[],'ymin':[],'xmax':[],'ymax':[],'ymax':[],'name':[] ,'label':[], 'image':[]}
-#     for file in sorted(glob.glob(str(Annotpath+'/*.xml*'))):
-#         dat=ET.parse(file)
-#         for element in dat.iter():    
-#             if 'object'==element.tag:
-#                 for attribute in list(element):
-#                     if 'name' in attribute.tag:
-#                         name = attribute.text
-#                         file_name = file.split('/')[-1][0:-4]
-#                         f = os.path.basename(file_name)
-#                         information['label'] += [name]
-#                         information['name'] +=[f+'.jpg']
-#                         information['image'] += [os.path.join(images,f+'.jpg')]
-#                     if 'bndbox'==attribute.tag:
-#                         for dim in list(attribute):
-#                             if 'xmin'==dim.tag:
-#                                 xmin=int(round(float(dim.text)))
-#                                 information['xmin']+=[xmin]
-#                             if 'ymin'==dim.tag:
-#                                 ymin=int(round(float(dim.text)))
-#                                 information['ymin']+=[ymin]
-#                             if 'xmax'==dim.tag:
-#                                 xmax=int(round(float(dim.text)))
-#                                 information['xmax']+=[xmax]
-#                             if 'ymax'==dim.tag:
-#                                 ymax=int(round(float(dim.text)))
-#                                 information['ymax']+=[ymax]
-#     return pd.DataFrame(information)
-
-
-# print("-------------------------------")
-# print("Converting XML files to dataframe.....")
-# print("-------------------------------")
-# df = generate_data(annots,images)
-# df.to_pickle(os.path.join(outputannot,'v{}.pkl'.format(params['dcount'])))
-
-
 import sys
 import os
 import yaml
@@ -66,6 +6,14 @@ import glob
 import xml.etree.ElementTree as ET
 
 params = yaml.safe_load(open('params.yaml'))
+
+if len(sys.argv) != 4:
+    sys.stderr.write('Arguments error. Usage:\n')
+    sys.stderr.write(
+        '\tpython3 src/transform.py data/prepared data/augmented data/transformed\n'
+    )
+    sys.exit(1)
+
 
 def creatingInfoData(Annotpath):
     xml_list = []
@@ -87,23 +35,39 @@ def creatingInfoData(Annotpath):
     return xml_df
 
 
-if len(sys.argv) != 4:
-    sys.stderr.write('Arguments error. Usage:\n')
-    sys.stderr.write(
-        '\tpython3 src/transform.py data/prepared data/augmented data/transformed\n'
-    )
-    sys.exit(1)
+def aug_img_df(Annotpath):
+    aug_list = []
+    for files in sorted(glob.glob(str(Annotpath+'/*.txt*'))):
+        with open(files, "r") as f:
+            bbox = (f.read()).split('\n')
+        for data in bbox:
+            data = data.split()
+            value = (
+                int(data[0]),
+                int(data[1]),
+                int(data[2]),
+                int(data[3]),
+                files,
+                int(data[4]),
+            )
+            aug_list.append(value)
+    column_name = ['xmin', 'ymin', 'xmax', 'ymax', 'name', 'label']
+    aug_df = pd.DataFrame(aug_list, columns = column_name)
+    return aug_df
 
-outputannot = os.path.join(sys.argv[3],f"v{params['ingest']['dcount']}")
-os.makedirs(outputannot, exist_ok = True)
 
-train_annot_path = params['train']['annot_path']
-test_annot_path = params['test']['annot_path']
+if __name__ == "__main__":
 
-train_output_annot = creatingInfoData(train_annot_path)
-test_output_annot = creatingInfoData(test_annot_path)
-print("-------------------------------")
-print("Tranforming.....")
-print("-------------------------------")
-train_output_annot.to_pickle(os.path.join(outputannot,'v{}_train.pkl'.format(params['ingest']['dcount'])))
-test_output_annot.to_pickle(os.path.join(outputannot,'v{}_test.pkl'.format(params['ingest']['dcount'])))
+    outputannot = os.path.join(sys.argv[3],f"v{params['ingest']['dcount']}")
+    os.makedirs(outputannot, exist_ok = True)
+
+    train_annot_path = params['train']['annot_path']
+    test_annot_path = params['test']['annot_path']
+
+    train_output_annot = creatingInfoData(train_annot_path)
+    test_output_annot = creatingInfoData(test_annot_path)
+    print("-------------------------------")
+    print("Tranforming.....")
+    print("-------------------------------")
+    train_output_annot.to_pickle(os.path.join(outputannot,'v{}_train.pkl'.format(params['ingest']['dcount'])))
+    test_output_annot.to_pickle(os.path.join(outputannot,'v{}_test.pkl'.format(params['ingest']['dcount'])))
